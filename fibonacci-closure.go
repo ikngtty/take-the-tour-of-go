4ed977562f2da0b5d1f6f1a9d5f2e8f4a8441181
ikngtty <ikngtty@gmail.com>
2018-12-06 17:47:10 +0900

    Add `fibonacci-closure`.

---
+++ fibonacci-closure.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+  prev := -1
+  current := 1
+	return func() int {
+		sum := current + prev
+		prev, current = current, sum
+		return sum
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
