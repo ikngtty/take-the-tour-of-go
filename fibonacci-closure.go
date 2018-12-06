10481d918b0fd3e1725e1d8c93ebd3d5b51562bc
ikngtty <ikngtty@gmail.com>
2018-12-06 18:01:02 +0900

    Use a hard tab for indentation.

--- fibonacci-closure.go
+++ fibonacci-closure.go
@@ -5,8 +5,8 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-  prev := -1
-  current := 1
+	prev := -1
+	current := 1
 	return func() int {
 		sum := current + prev
 		prev, current = current, sum
